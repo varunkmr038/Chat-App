d67b6936ecc3cc524e8cc5a11bdced0f209c66f4
varun <varunkmr038@gmail.com>
2021-02-12 17:53:48 +0530

    f

--- static/js/client.js
+++ static/js/client.js
@@ -1,4 +1,4 @@
-const socket=io('http://localhost:8000');
+const socket=io('https://ichatweb.herokuapp.com/');
 
 const form=document.getElementById('send');
 const messageinp=document.getElementById('inp-message');
